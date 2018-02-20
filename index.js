03ae07c60edb79e9a0d680fdea850f72d7a88ba6
Pamela Argentino <pamela.oliveira@cognizant.com>
2018-02-20 14:41:23 -0300

    Execute Database Connection step

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config({ silent: true });
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Successfully connected to MongoDB.');
+    app.listen(8080, () => console.log('Express server listening on port 8080...'));
+  })
+  .catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+  });
 
-app.listen(8080, () => console.log('Express server listening on port 8080'));
+app.get('/', (req, res) => res.send('Hello World!'));
